98f8be46d6bb433d47fa26b97262e63f9bc672fc
Michelle Wong <michellekwong2@gmail.com>
2020-01-23 11:29:53 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -9,10 +9,15 @@ function changeState(state, action){
     }
   }
  
+//ensures each time state updates, our HTML updates to reflect these changes
 function render(){
     document.body.textContent = state.count
 }
  
+//persists changes to state
+  //bc we called dispatch function, the dispatch function called our reducer, 
+    // & then we took the return value from the reducer & assigned it to be the new state
+
 function dispatch(action){
   state = changeState(state, action)
   render()
